c7fe061e281ba92db9a6d041b5cf04f0c9e5064b
aleyii <ahkrayem@hotmail.com>
2017-11-26 18:53:29 -0500

    Add files via upload

---
+++ Controllers/LogInController.java
@@ -0,0 +1,28 @@
+package application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LogInController implements Initializable {
+
+    public void logInButton(ActionEvent event) throws IOException{
+        Parent manager_home_parent = FXMLLoader.load(getClass().getResource("storeManagerHomePage.fxml"));
+        Scene manager_home_scene = new Scene (manager_home_parent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(manager_home_scene);
+        app_stage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ Controllers/removeItemController.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class removeItemController {
+	private Parent manager_home_parent;
+	 public void ReturnInven(ActionEvent event) throws IOException {
+
+	       manager_home_parent = FXMLLoader.load(getClass().getResource("storeInventory.fxml"));
+	        Scene manager_home_scene = new Scene (manager_home_parent);
+	        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	        app_stage.setScene(manager_home_scene);
+	        app_stage.show();
+	    }
+
+}
