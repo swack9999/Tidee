60484e3241c09290ab796daa12fb79f7324705fb
monzur31 <33733916+monzur31@users.noreply.github.com>
2017-11-28 09:38:47 -0500

    Update Main.java

--- Login controller working with SQL/Main.java
+++ Login controller working with SQL/Main.java
@@ -1,58 +1,40 @@
 package Tidee;
-
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.Statement;
-
+	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.*;
+
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-    	//connectToDB();
-        Parent root = FXMLLoader.load(getClass().getResource("LogIN.fxml"));
-        primaryStage.setTitle("TIDEE");
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("logIN.fxml"));
         primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("Tidee");
         primaryStage.show();
-
     }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
-    
-    /*public void connectToDB() {
-   
-    		try {
-    			// Get a connection
-    			Connection myConn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/stocksystem?autoReconnect=true&useSSL=false", "root", "MyNewPass");
-    			// Create a statement
-    			Statement myStmt = (Statement) myConn.createStatement();
-    			//Execute Query
-    			ResultSet myResult = myStmt.executeQuery("select * from item");
-    			while (myResult.next())
-    			{
-    				System.out.println('$' + myResult.getString("price") + ", " +  myResult.getString("itemName"));
-    				System.out.println("Product Description: " + myResult.getString("productDescription"));
-    			}
-    			myResult = myStmt.executeQuery("select * from employee");
-    			while (myResult.next())
-    			{
-    				System.out.println('$' + myResult.getString("fName") + ", " +  myResult.getString("empType"));
-    			}
-    			
-    		}
-    		catch (Exception exc) {
-    			exc.printStackTrace();
-    		}
-    	}*/
-    
+	
+	public static void main (String [] args ) {
+		
+		try {
+			// Get a connection
+			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/stocksystem?autoReconnect=true&useSSL=false", "root", "hello123");
+			// Create a statement
+			Statement myStmt = myConn.createStatement();
+			//Execute Query
+			ResultSet myResult = myStmt.executeQuery("select * from item");
+			
+			while (myResult.next())
+			{
+				System.out.println('$' + myResult.getString("price") + ", " +  myResult.getString("itemName"));
+			}
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		launch(args);
+	}
 }
